244e7e5ede782769961dc36ed86957de043fee50
Orkhan <orhan2@bk.ru>
2024-02-26 18:01:24 +0400

    feat: Add pulling of data from API

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -1,38 +1,52 @@
+import md5 from "md5";
+
+const timestamp = new Date().toISOString().slice(0, 10).replace(/-/g, '');
+const password = 'Valantis';
+const authString = md5(`${password}_${timestamp}`);
+
+
 async function getProductIds(params) {
-    const response = await fetch('https://example.com/api', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        action: "get_ids",
-        params: params
-      }),
+    console.log(params)
+    const response = await fetch('http://api.valantis.store:40000/', {
+        method: 'POST',
+        headers: {
+            'X-Auth': authString,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "action": "get_ids",
+            "params": { "offset": params.offset, "limit": params.limit }
+        }),
     });
-  
+
     if (!response.ok) {
-      throw new Error('Ошибка при получении ID товаров');
+        throw new Error('Ошибка при получении ID товаров');
     }
-  
+
     const data = await response.json();
-    return data.result; 
-  }
-  
-  async function getProductsByIds(ids) {
-    const response = await fetch('https://example.com/api', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        action: "get_items",
-        params: { ids }
-      }),
+    return data.result;
+}
+
+async function getProductsByIds(ids) {
+    const response = await fetch('http://api.valantis.store:40000/', {
+        method: 'POST',
+        headers: {
+            'X-Auth': authString,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "action": "get_items",
+            "params": {"ids": ids}
+        }),
     });
-  
+
     if (!response.ok) {
-      throw new Error('Ошибка при получении информации о товарах');
+        throw new Error('Ошибка при получении информации о товарах');
     }
-  
+
     const data = await response.json();
-    return data.result; 
-  }
-  
-  export { getProductIds, getProductsByIds };
-  
+    console.log(data)
+    return data.result;
+}
+
+export { getProductIds, getProductsByIds };
--- src/components/Table.js
+++ src/components/Table.js
@@ -7,8 +7,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import md5 from 'md5';
 import { TablePagination } from '@mui/material';
+import { getProductIds, getProductsByIds } from '../api/productsApi';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -30,60 +30,47 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }));
 
 function CustomizedTables() {
-    const [page, setPage] = React.useState(0);
-  
-    const [rows, setRows] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage] = useState(50);
+    const [offset, setOffset] = useState(0);
+    const [allRows, setAllRows] = useState([]);
+    const [displayRows, setDisplayRows] = useState([]);
+
 
     const handleChangePage = (event, newPage) => {
+        const newOffset = newPage * rowsPerPage;
+        setOffset(newOffset);
         setPage(newPage);
-      };
+        setDisplayRows(allRows.slice(newOffset, newOffset + rowsPerPage));
+    };
+
 
     useEffect(() => {
         const fetchProducts = async () => {
-            const timestamp = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-            const password = 'Valantis';
-            const authString = md5(`${password}_${timestamp}`);
-
             try {
-                const response = await fetch('http://api.valantis.store:40000/', {
-                    method: 'POST',
-                    headers: {
-                        'X-Auth': authString,
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        "action": "get_ids",
-                        "params": {"offset": 10, "limit": 50}
-                    }),
-                });
-
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                if (allRows.length <= offset + rowsPerPage) {
+                    const ids = await getProductIds({ offset: allRows.length, limit: 100});
+                    const uniqueIds = ids.filter(id => !allRows.find(row => row.id === id));
+                    const productsData = await getProductsByIds(uniqueIds);
+                    const newAllRows = [...allRows, ...productsData].reduce((acc, current) => {
+                        const x = acc.find(item => item.id === current.id);
+                        if (!x) {
+                            return acc.concat([current]);
+                        } else {
+                            return acc;
+                        }
+                    }, []);
+                    setAllRows(newAllRows);
                 }
-                const data = await response.json();
-
-                const resResponse = await fetch('http://api.valantis.store:40000/', {
-                    method: 'POST',
-                    headers: {
-                        'X-Auth': authString,
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        "action": "get_items",
-                        "params": data.result
-                    }),
-                });
-                const resData = await response.json();
-
-                setRows();
-
+                setDisplayRows(allRows.slice(offset, offset + rowsPerPage));
             } catch (error) {
                 console.error("Fetching error:", error);
             }
         };
 
         fetchProducts();
-    }, []);
+    }, [offset, allRows]);
+
 
     return (
         <TableContainer component={Paper}>
@@ -97,23 +84,24 @@ function CustomizedTables() {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <StyledTableRow key={row.name}>
+                    {displayRows.map((row) => (
+                        <StyledTableRow key={row.id}>
                             <StyledTableCell component="th" scope="row">
                                 {row.id}
                             </StyledTableCell>
-                            <StyledTableCell align="right">{row.name}</StyledTableCell>
-                            <StyledTableCell align="right">{row.cost}</StyledTableCell>
+                            <StyledTableCell align="right">{row.product}</StyledTableCell>
+                            <StyledTableCell align="right">{row.price}</StyledTableCell>
                             <StyledTableCell align="right">{row.brand}</StyledTableCell>
                         </StyledTableRow>
                     ))}
                 </TableBody>
+
             </Table>
             <TablePagination
                 rowsPerPageOptions={[50]}
                 component="div"
-                count={rows.length}
-                rowsPerPage={50}
+                count={-1}
+                rowsPerPage={rowsPerPage}
                 page={page}
                 onPageChange={handleChangePage}
             />
